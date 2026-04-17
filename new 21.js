ff7bb0efa7aa13f331c66eec668a875529724909
ke8816174-sketch <ke8816174@gmail.com>
2026-04-17 12:53:23 +0500

    Add files via upload

---
+++ new 21.js
@@ -0,0 +1,32 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/30800729acfbcb452314a6e60a65411e.jpg",
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/312c676857e037e0809373f1217a7208.jpg",
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/5f5e96a98e249d1b782bc112d60bfe2b.jpg",
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/306d25e8234ebacf4d19a46b46df0492.jpg",
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/ab0d71d03afc1702d61f8be61a441974.jpg",
+
+  "https://raw.githubusercontent.com/ke8816174-sketch/KuznetsovaElizaveta202/refs/heads/main/d36f0d61ad1b0778360187bbe2cb551f.jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+
+const FACE_NAMES = [
+
+  "БУКЕТ",
+
+  "ДЕЛЬФИНИУМ",
+
+  "ИРИСЫ",
+
+  "КАЛЛЫ",
+
+  "РАНЮНКУЛУСЫ",
+
+  "ГОРТЕНЗИИ"
+
+];
